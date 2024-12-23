057624718c7df608435893dfac709c9d4d83ccac
Dalia Nezzar <nezzar.dalia@gmail.com>
2024-12-23 16:31:30 +0000

    add: ecosystem.config for the api

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{
+    name: "uberuberapi",
+    script: "bun",
+    args: "src/index.ts",
+    env: {
+      PORT: process.env.PORT || 3000,
+      DB_HOST: process.env.DB_HOST,
+      DB_USER: process.env.DB_USER,
+      DB_PASSWORD: process.env.DB_PASSWORD,
+      DB_NAME: process.env.DB_NAME
+    }
+  }]
+}
