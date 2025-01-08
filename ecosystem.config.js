bc9ad5b162335213f2595bb1e438918a2c6db9c1
Dalia Nezzar <113182098+dalia-nezzar@users.noreply.github.com>
2025-01-08 15:57:17 +0100

    fix: changed port j'suis nrv

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: "bun",
     args: "src/index.ts",
     env: {
-      PORT: process.env.PORT || 3001,
+      PORT: process.env.PORT || 3000,
       DB_HOST: process.env.DB_HOST,
       DB_USER: process.env.DB_USER,
       DB_PASSWORD: process.env.DB_PASSWORD,
